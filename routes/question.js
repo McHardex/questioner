67f8496147e20db13102860f147cc4541dd2caa7
mchardex <mchardex@gmail.com>
2018-12-23 17:19:20 +0100

    created route for fetching questions and passing test

--- routes/question.js
+++ routes/question.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 const db = require('../startup/db/questionDb');
 
+router.get('/questions', (req, res) => {
+  res.status(200).send({
+    status: 200,
+    data: db,
+  });
+});
+
 router.post('/questions', async (req, res) => {
   if (!req.body.title) return res.status(400).send({ message: 'title is required' });
   if (!req.body.body) return res.status(400).send({ message: 'body is required' });

aaaae44b8f1592eeae61aa4611291b2658b17dea
mchardex <mchardex@gmail.com>
2018-12-26 18:51:10 +0100

    clean up develop branch

---
+++ routes/question.js
@@ -0,0 +1,37 @@
+/* eslint-disable consistent-return */
+
+import express from 'express';
+import db from '../startup/db/db';
+import getEndpointControllers from '../controller/getAllEndpoint';
+import patch from '../controller/patchRequest';
+
+const router = express.Router();
+const meetupId = 1;
+
+router.get('/questions', getEndpointControllers.getAllQuestions);
+
+router.post('/questions', async (req, res) => {
+  if (!req.body.title) return res.status(400).send({ message: 'title is required' });
+  if (!req.body.body) return res.status(400).send({ message: 'body is required' });
+
+  const question = {
+    userId: 1,
+    meetupId,
+    createdOn: new Date(),
+    title: req.body.title,
+    body: req.body.body,
+    votes: 0,
+  };
+
+  await db.questionDb.push(question);
+  res.status(201).send({
+    status: 201,
+    data: [question],
+  });
+});
+
+router.patch('/questions/:question_id/upvote', patch.upvote);
+
+router.patch('/questions/:question_id/downvote', patch.downvote);
+
+export default router;

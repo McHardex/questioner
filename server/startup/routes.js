4baa8dd0db8eac806c7cf9e6f137cb82401d7785
mchardex <mchardex@gmail.com>
2019-01-25 18:31:36 +0100

    Feat(frontend)- implement user sign up and login user should be able to sign up and login with valid credentials

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from '../routes/routes';
 import invalidUrl from '../middlewares/invalidUrl';
 
 module.exports = (app) => {
   // parse incoming requests data
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
 

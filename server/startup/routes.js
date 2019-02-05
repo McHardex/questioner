d01879653cd02bf12aaadd0b496f030d75170710
mchardex <mchardex@gmail.com>
2019-02-05 20:24:12 +0100

    cors setup

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, x-auth-token, Content-Type, Accept');
     next();
   });
   app.use('/api/v1', routes);

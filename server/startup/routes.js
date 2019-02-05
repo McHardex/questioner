9134ecf9338a6502701841efae49d0a4136b31ca
mchardex <mchardex@gmail.com>
2019-02-05 20:56:20 +0100

    cors setup

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -8,6 +8,7 @@ module.exports = (app) => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, x-auth-token, Content-Type, Accept');
     next();
   });

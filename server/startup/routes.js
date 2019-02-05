7aeba88af591c9f3b8774dcfaf9f8e8bdeccde71
mchardex <mchardex@gmail.com>
2019-02-05 19:55:39 +0100

    cors setup

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -6,6 +6,11 @@ import invalidUrl from '../middlewares/invalidUrl';
 module.exports = (app) => {
   // parse incoming requests data
   app.use(cors());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
 

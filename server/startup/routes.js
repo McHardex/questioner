fce4ba0eeac99979779b0eca3c22f73cdee8c098
mchardex <mchardex@gmail.com>
2019-02-05 20:06:56 +0100

    cors setup

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,19 +1,16 @@
 import bodyParser from 'body-parser';
-import cors from 'cors';
 import routes from '../routes/routes';
 import invalidUrl from '../middlewares/invalidUrl';
 
 module.exports = (app) => {
   // parse incoming requests data
-  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-
   app.use('/api/v1', routes);
   app.use(invalidUrl);
 };
